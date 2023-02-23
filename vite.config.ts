5eddcf47cbcaa4b06b5c2aecd62b2859189e3fbe
Fred Lavoie <flavoie3@gmail.com>
2023-02-23 08:43:32 -0500

    Additional config settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
     },
     server: {
         port: 3000,
+    },
+    resolve: {
+        alias: {
+            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+        },
     },
 });
